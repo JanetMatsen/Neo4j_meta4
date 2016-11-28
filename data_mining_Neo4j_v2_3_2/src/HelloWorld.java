271e96be84c1d8f35b3c3f61232fb17e21c748b0
Janet Matsen <janetmatsen@gmail.com>
2016-11-28 05:50:08 -0800

    made a HelloWorld jar to test waffle

---
+++ data_mining_Neo4j_v2_3_2/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+    // http://introcs.cs.princeton.edu/java/11hello/HelloWorld.java.html
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
